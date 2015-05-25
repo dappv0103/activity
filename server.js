aea6672bd18826753ff5fe4262fa4c22f7821ae5
Nguyen Thang <thangn.1411@gmail.com>
2015-05-25 19:32:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+var net = require('net');
+var server = net.createServer(function(socket) { //'connection' listener
 
+
+  console.log('client connected');
+  
+  socket.on('data', function(data) {
+    
+  });
+  
+  socket.on('end', function() {
+    console.log('client disconnected');
+  });
+  
+});
+server.listen(8124, function() { //'listening' listener
+  console.log('server bound');
+});
