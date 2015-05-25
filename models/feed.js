17cd35a790c3274d4f964b549bc4a60ef1514d5d
Nguyen Thang <thangn.1411@gmail.com>
2015-05-25 21:47:54 +0700

    Update feed.js

--- models/feed.js
+++ models/feed.js
@@ -9,6 +9,12 @@ Feed.prototype.add = function(users, data) {
   
 };
 
+Feed.prototype.remove = function(query) {
+  this.mongo.find(query, function(err,reply) {
+    reply.delete();
+  });
+};
+
 Feed.prototype.get = function (query, callback) {
   this.mongo.find({hash: query}, function(err, results) {
     return callback(err, results);
