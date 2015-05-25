e27ca488574fc5fca448213e6e94f5edb984e85e
Nguyen Thang <thangn.1411@gmail.com>
2015-05-25 20:19:32 +0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,16 @@
 
+
+module.exports = User;
+
+
+function User() {
+  
+};
+
+User.prototype.find(query, callback) {
+  if(var user = query.following) {
+    Redis.zrange('following_user' + user, function(err, reply) {
+      return callback(reply);
+    });
+  }
+}
