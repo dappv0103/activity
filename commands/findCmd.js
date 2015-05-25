d985413afbc048f0e2acd7ddf5015744e3993409
Nguyen Thang <thangn.1411@gmail.com>
2015-05-25 20:52:18 +0700

    Update findCmd.js

--- commands/findCmd.js
+++ commands/findCmd.js
@@ -1,1 +1,32 @@
+var Feed = require('../models/feed');
+var Notification = require('../models/notification');
+var baseCmd = require('./baseCmd');
 
+module.exports = findCmd;
+
+
+function findCmd() {
+  this.data = null;
+  this.error = null;
+};
+
+findCmd.prototype = baseCmd;
+
+findCmd.prototype.run = function (data, callback) {
+  var self = this;
+  if(data.type === 'feed') {
+    Feed.get(data, function(results) {
+      self.callCmd(results, callback);
+    });
+  } else(data.type === 'notify') {
+    Notification.get(data, function(results) {
+      self.callCmd(results, callback);
+    });
+  }
+ 
+};
+
+findCmd.prototype.callCmd = function(results, callback) {
+  self.data = results;
+  callback(self.getString());
+};
