9bd6910870b22f90b2a9f63daa61105be0e3ce42
Nguyen Thang <thangn.1411@gmail.com>
2015-05-25 20:30:08 +0700

    Update removeCmd.js

--- commands/removeCmd.js
+++ commands/removeCmd.js
@@ -1,1 +1,19 @@
+var Feed = require('../models/feed');
+var Notification = require('../models/notification');
 
+module.exports = removeCmd;
+
+
+function removeCmd() {
+  this.data = null;
+  this.error = null;
+};
+
+removeCmd.prototype.run(data) {
+  Feed.remove(data);
+  Notification.remove(data);
+  this.data = {
+    result:'ok'
+  };
+  return this;
+};
