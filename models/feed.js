b439ce8dafe4fcfc7c546fb915ea6070bb9394f9
Nguyen Thang <thangn.1411@gmail.com>
2015-05-25 21:09:17 +0700

    Update feed.js

--- models/feed.js
+++ models/feed.js
@@ -1,1 +1,12 @@
+var Mongo = require('../db/mongo');
+module.exports = Feed;
 
+function Feed() {
+  this.mongo = Mongo.collenction('newsfeed');
+};
+
+Feed.get = function (query, callback) {
+  this.mongo.find({hash: query}, function(err, results) {
+    return callback(err, results);
+  });
+};
