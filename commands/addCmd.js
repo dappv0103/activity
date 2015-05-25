205b871f3c412e051c57835c99b421900465edcd
Nguyen Thang <thangn.1411@gmail.com>
2015-05-25 19:55:04 +0700

    Update addCmd.js

--- commands/addCmd.js
+++ commands/addCmd.js
@@ -1,1 +1,23 @@
 
+
+module.exports addCmd;
+
+
+function addCmd() {
+  this.data = "";
+};
+
+addCmd.prototype.run(data) {
+  return this;
+};
+
+addCmd.prototype.getResult() {
+  if(!this->data) {
+    
+  }  
+  return data;
+};
+
+addCmd.prototype.getString() {
+  return JSON.stringify(this.getResult());
+};
