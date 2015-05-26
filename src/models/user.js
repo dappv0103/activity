0cebf170695fa51e403b305d34505088dc708fcd
Nguyen Thang <thangn.1411@gmail.com>
2015-05-26 20:31:56 +0700

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -11,13 +11,13 @@ function User() {
 User.prototype.find = function(query, callback) {
   if(var user = query.following) {
     // newsfeed
-    Redis.zrange('following_user' + user, function(err, reply) {
+    this.redis.zrange('following_user' + user, function(err, reply) {
       return callback(reply);
     });
   } else if(var user = query.notify) {
     
     // notification
-    Redis.zrange('notification_from_' + user, function(err, reply) {
+    this.redis.zrange('notification_from_' + user, function(err, reply) {
       return callback(reply);
     });
   };
