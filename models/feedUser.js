674fff8a0aef845850403edb169d398146a782ce
Nguyen Thang <thangn.1411@gmail.com>
2015-05-26 10:57:19 +0700

    Update feedUser.js

--- models/feedUser.js
+++ models/feedUser.js
@@ -1,1 +1,20 @@
 
+
+
+
+function feedUser() {
+  this.user_id = user_id;
+};
+
+
+
+feedUser.prototype.create = function(user_id) {
+  this.collection(function(collection) {
+    collection.insert({
+      user_id: user_id,
+      items: []
+    });
+  });
+}
+
+
