94354b94a044aca5c13c3d76352bcb6ae6c33add
Nguyen Thang <thangn.1411@gmail.com>
2015-05-27 11:17:24 +0700

    Create channel.js

---
+++ src/models/channel.js
@@ -0,0 +1,23 @@
+
+function Channel() {
+	this.id = null;
+	this.channel = null;
+	this.feeds = [];
+	this.notifications = [];
+}
+
+Channel.prototype.add = function(name) {
+	
+	var channel = new Channel();
+	channel.channel = name;
+	return channel.insert();
+}
+
+Channel.prototype.set = function(field, data) {
+	this[field] = data;
+	return this.update();
+}
+
+Channel.prototype.get = function(field) {
+	return this.find(field);
+}
