05021d364d73ce50f829dbd36a849c3d59813b77
Nguyen Thang <thangn.1411@gmail.com>
2015-05-27 11:37:22 +0700

    Update feed.js

--- src/models/feed.js
+++ src/models/feed.js
@@ -1,35 +1,24 @@
-var Mongo = require('../db/mongo');
-var feedUser = require('./feedUser');
+
 module.exports = Feed;
 
 
 function Feed() {
   if (!(this instanceof Feed)) return new Feed();
-  this.collectionName = 'newsfeed';
-  this.db = Mongo.getDb();
 };
 
-Feed.prototype.collection = function(callback) {
-  this.db.collection(this.collectionName, function(err, collection) {
-    return callback(collection);
-  });
-};
+Feed.prototype.channel = function(channel) {
+  return channel.channel(channel);
+}
+
+
 
 Feed.prototype.add = function(users, data) {
   var self = this;
   var new_data = data;
-  this.collection(function(collection) {
-    for(var i =0; i < users.length; i++) {
-      new_data.user_id = users[i];
-      self.push(collection, new_data);
-    }
-  });
-};
-
-Feed.prototype.push = function(collection, new_data) {
-  collection.insert(new_data, function(err, result) {
-    feedUser.channel(result.user_id).updateVersion();
-  });
+  for(var i =0; i < users.length; i++) {
+    new_data.user_id = users[i];
+    this.channel.add(channel.NEWS_FEED, new_data);
+  }
 };
 
 Feed.prototype.remove = function() {
