6ed944f079c47cebf770e4c945aac0dd5cfa4b4e
Nguyen Thang <thangn.1411@gmail.com>
2015-05-29 05:06:05 +0000

    update

--- src/models/channel.js
+++ src/models/channel.js
@@ -1,3 +1,4 @@
+var Redis = require('../models/redis');
 
 /*
 var channel =  Channel.channel(10);
@@ -27,13 +28,18 @@ channel.remove('notification', {
 channel.get('notification');
 */
 
-
+/**
+ * Channel
+ * 
+ */
 function Channel() {
 	this.channel = null;
 	this.keyPrefix = "Channel:";
 }
 
-// Connect to channel
+/**
+ * Connect channel
+ */
 Channel.prototype.channel = function(name) {
 	this.channel = name;
 	return this;
@@ -47,7 +53,6 @@ Channel.prototype.channel = function(name) {
 Channel.prototype.add = function(activity) {
 	var users = activity.to_id;
 	delete activity.to_id;
-	var 
 	for(var i = 0; i < users.length; i++) {
 		activity.to_id = users[i].id;
 		activity.hash = this._buildHash(activity);
@@ -79,9 +84,14 @@ Channel.prototype.insertOrUpdate = function(type, activity) {
 	}
 }
 
+
+/**
+ * Insert or update notification
+ */
 Channel.prototype.insertOrUpdateNotification = function(activity) {
 	var self = this;
 	var hash = 'notification:' + activity.hash;
+	
 	this.getCacheItem(hash, function(err, reply) {
 		if(!reply) {
 			reply = activity;
@@ -89,14 +99,90 @@ Channel.prototype.insertOrUpdateNotification = function(activity) {
 			reply.actor.total += 1;
 			reply.actor._new.push(activity.actor);
 		}
-		this.setCacheItem(hash, activity);
+		self.setCacheItem(hash, activity);
+		self.channel(activity.to_id).addNotificationItem(hash);
 	});
 }
 
-Channel.prototype.setCacheItem = function(hash, value) {
-	Redis.set(this.keyPrefix +':'+hash, value);
+/**
+ * Insert or update newsfeed
+ */
+Channel.prototype.insertOrUpdateNewsfeed = function(activity) {
+	var self = this;
+	var hash = 'newsfeed:' + activity.hash;
+	this.getCacheItem(hash, function(err, reply) {
+		if(!reply) {
+			reply = activity;
+		} else {
+			reply.actor.total += 1;
+			reply.actor._new.push(activity.actor);
+		}
+		self.setCacheItem(hash, activity);
+		self.channel(activity.to_id).addNewsfeedItem(hash);
+	});
 }
 
+/**
+ * Get Newsfeed
+ */
+Channel.prototype.getNewsFeed = function(callback) {
+	return Redis.zrange('newsfeed_item:' + this.channel, function(err, reply) {
+		callback(reply);
+	});
+}
+
+/**
+ * Get Notification
+ */
+Channel.prototype.getNotification = function(callback) {
+	return Redis.zrange('notification_item:' + this.channel, function(err, reply) {
+		callback(reply);
+	});
+}
+
+/**
+ * Add Newsfeed Item
+ */
+Channel.prototype.addNewsfeedItem = function(value, score) {
+	return Redis.zset('newsfeed_item:' + this.channel, score, value);
+}
+
+/**
+ * Add Notification Item
+ */
+Channel.prototype.addNotificationItem = function(value, score) {
+	return Redis.zset('notification_item:' + this.channel, score, value);
+}
+
+/**
+ * Remove notification item
+ */
+Channel.prototype.removeNotificationItem = function(value) {
+	return Redis.zrem('notification_item:' + this.channel, value);
+}
+
+/**
+ * Remove newsfeed item
+ */
+Channel.prototype.removeNewsfeedItem = function(value) {
+	return Redis.zrem('newsfeed_item:' + this.channel, value);
+}
+
+/**
+ * Set Cache Item
+ * @param string hash
+ * @param value
+ * @return integer
+ */
+Channel.prototype.setCacheItem = function(hash, value) {
+	return Redis.set(this.keyPrefix +':'+hash, value);
+}
+
+/**
+ * Get cache template
+ * @param string hash
+ * @param function callback
+ */
 Channel.prototype.getCacheItem = function(hash, callback) {
 	Redis.get(this.keyPrefix +':'+hash, callback);
 }
