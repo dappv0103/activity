2e4f5001e968eca166268a3144b5c14b93ecdf80
Nguyen Thang <thangn.1411@gmail.com>
2015-06-01 13:37:40 +0700

    Update feed.js

--- src/models/feed.js
+++ src/models/feed.js
@@ -23,7 +23,7 @@ Feed.prototype.add = function(data) {
   
   // add newsfeed
   var keyPrefix = this.keyPrefix+':'+this._channelName;
-  redis.zadd(key, data.rank, data.object);
+  redis.zadd(keyPrefix, data.rank, data.object);
   
   // actors
   key = keyPrefix + ':' + data.object + ':' + data.verb;
@@ -34,6 +34,23 @@ Feed.prototype.add = function(data) {
   redis.zadd(key, Date.now(), data.verb);
 }
 
-Feed.prototype._addActivity = function(data) {
-  
+Feed.prototype.find = function(condition) {
+  var _results = [];
+  var keyPrefix = this.keyPrefix+':'+this._channelName;
+  var key = null;
+  redis.zrange(keyPrefix,  0, -1, function(err, replies) {
+    for(var i = 0; i <= replies.length, i++) {
+      var result = {
+        object: replies[i],
+      }
+      key = keyPrefix + ':' + replies[i] + ':verbs';
+      redis.zrange(key, 0, 1, function(err, reply) {
+        key = keyPrefix + ':' + result.object + ':' + reply;
+        result.verb = reply[0];
+        redis.zrange(key, 0, -1, function(err, users) {
+          result.receivers = users;
+        });
+      });
+    }
+  });
 }
