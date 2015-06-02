92b9cceb476d5aa5fe10cf2f6a73f276089b9225
Nguyen Thang <thangn.1411@gmail.com>
2015-06-02 15:20:51 +0700

    Update alert_map.js

--- src/models/alert_map.js
+++ src/models/alert_map.js
@@ -1,1 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
+
+/**
+ * @var string _id
+ * @var integer user_id
+ * @var object object
+ */
+ 
+ 
+
+
+var AlertMap = new Schema({
+  user_id: Number,
+  object:  {
+    type: String,
+    id: Number
+  },
+});
+
+mongoose.model('AlertMap', AlertMap);
