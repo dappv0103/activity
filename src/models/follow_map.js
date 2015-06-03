25aeb3e14fa7e114b27769bc9fd27b86491f64d8
Nguyen Thang <thangn.1411@gmail.com>
2015-06-03 10:28:49 +0700

    Update follow_map.js

--- src/models/follow_map.js
+++ src/models/follow_map.js
@@ -1,1 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
+
+/**
+ * @var string _id
+ * @var integer user_id
+ * @var object object
+ */
+ 
+ 
+
+
+var FollowMap = new Schema({
+  user_id: Number,
+  object:  {
+    type: String,
+    id: Number
+  },
+  ranking: Number
+});
+
+mongoose.model('FollowMap', FollowMap);
