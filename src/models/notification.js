75eb6cd03a8b951f9d6cf0b157446b1572e42310
Nguyen Thang <thangn.1411@gmail.com>
2015-06-03 10:35:28 +0700

    Update notification.js

--- src/models/notification.js
+++ src/models/notification.js
@@ -19,11 +19,33 @@ var Schema = mongoose.Schema;
 
 var NotificationSchema = new Schema({
   to_id: Number,
-  actor: Number,
+  actors: Array,
   verb:  Schema.Types.Mixed,
   feed_id:  Schema.Types.ObjectId,
-  is_read: Boolean,
+  is_read: {
+   type: Boolean,
+   default: false,
+  },
   created_at: { type: Date, default: Date.now },
 });
+
+NotificationSchema.insertOrUpdate = function(data) {
+ var self = this;
+ this.findOne({feed_id: data.feed_id, verb: data.verb}, function(err, doct) {
+  if(!doct ) {
+   self.insert({
+    to_id: data.to_id,
+    actors: [data.actor],
+    verb: data.verb,
+    feed_id: data.feed_id,
+   });
+  } else {
+   doct.actors.push(data.actor);
+   doct.save(function(err) {
+    // log error
+   });
+  }
+ });
+}
 
 mongoose.model('Notification', NotificationSchema);
