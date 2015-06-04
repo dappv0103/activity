418a92c9184382cc321e3096232db455760d7a50
Nguyen Thang <thangn.1411@gmail.com>
2015-06-04 15:54:00 +0700

    Update newsfeed_cmd.js

--- src/commands/newsfeed_cmd.js
+++ src/commands/newsfeed_cmd.js
@@ -1,35 +1,23 @@
 var Channel = require('../models/channel');
 var baseCmd = require('./baseCmd');
 
-module.exports = findCmd;
+module.exports = Newsfeed;
 
 
-function findCmd() {
-  if (!(this instanceof findCmd)) return new findCmd();
+function Newsfeed() {
+  if (!(this instanceof Newsfeed)) return new Newsfeed();
   this.data = null;
   this.error = null;
 };
 
-findCmd.prototype = baseCmd;
+Newsfeed.prototype = baseCmd;
 
-findCmd.prototype.run = function (data, callback) {
+Newsfeed.prototype.run = function (data, callback) {
   var self = this;
-  if(data.type === 'feed') {
-    
-    Channel.channel(data.user_id).getNewsfeed(function(results) {
-      self.callCmd(results, callback);
-    });
-    
-  } else if(data.type === 'notify') {
-    
-    Channel.channel(data.user_id).getNotification(function(results) {
-      self.callCmd(results, callback);
-    });
-  }
- 
+  
 };
 
-findCmd.prototype.callCmd = function(results, callback) {
+Newsfeed.prototype.callCmd = function(results, callback) {
   this.data = results;
   callback(this.getString());
 };
