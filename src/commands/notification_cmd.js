58e4b8c3693b6b6b45390f0d4397019de6f9e048
Nguyen Thang <thangn.1411@gmail.com>
2015-06-04 16:24:53 +0700

    Update notification_cmd.js

--- src/commands/notification_cmd.js
+++ src/commands/notification_cmd.js
@@ -1,1 +1,37 @@
+var mongoose = require('mongoose');
+var Feed = mongoose.model('Feed');
+var baseCmd = require('./base_cmd');
 
+module.exports = NotificationCmd;
+
+
+function NotificationCmd() {
+  if (!(this instanceof NotificationCmd)) return new NotificationCmd();
+  this.data = null;
+  this.error = null;
+};
+
+NotificationCmd.prototype = baseCmd;
+
+/**
+ * Thêm bảng tin mới
+ * 
+ * @param Object    data {verb, actor, object}
+ * @param Function  callback
+ */
+NotificationCmd.prototype.run = function (data, callback) {
+  
+  var self = this;
+  
+  // Thêm hoạt động mới
+  Feed.activity(
+      data.verb, 
+      data.actor, 
+      data.object
+  );
+  
+  this.data = {
+    result: 'ok'
+  };
+  callback(this.getString());
+};
