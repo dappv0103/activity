bafd1f9d0ca2fe69bdb826fc99e86d5af1e16b12
Nguyen Thang <thangn.1411@gmail.com>
2015-06-04 16:20:54 +0700

    Update delete_activity_cmd.js

--- src/commands/delete_activity_cmd.js
+++ src/commands/delete_activity_cmd.js
@@ -1,1 +1,37 @@
+var mongoose = require('mongoose');
+var Feed = mongoose.model('Feed');
+var baseCmd = require('./base_cmd');
 
+module.exports = DeleteActivityCmd;
+
+
+function DeleteActivityCmd() {
+  if (!(this instanceof DeleteActivityCmd)) return new DeleteActivityCmd();
+  this.data = null;
+  this.error = null;
+};
+
+DeleteActivityCmd.prototype = baseCmd;
+
+/**
+ * Xóa hành động đến đối tượng
+ * 
+ * @param Object    data {verb, actor, object}
+ * @param Function  callback
+ */
+DeleteActivityCmd.prototype.run = function (data, callback) {
+  
+  var self = this;
+  
+  // Thêm hoạt động mới
+  Feed.removeActivity(
+      data.verb, 
+      data.actor, 
+      data.object
+  );
+  
+  this.data = {
+    result: 'ok'
+  };
+  callback(this.getString());
+};
