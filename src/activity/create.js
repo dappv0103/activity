ab1c76983b4123e9640215182cd676a99108e399
Nguyen Thang <thangn.1411@gmail.com>
2015-06-05 13:46:08 +0700

    Update create.js

--- src/activity/create.js
+++ src/activity/create.js
@@ -3,4 +3,11 @@ function Create(activity) {
   this.created_by = activity.actor;
   this.object = activity.object;
   this.meta = activity.meta;
+  this.createFeed();
+}
+
+Create.prototype.createFeed = function() {
+  Feed.create({
+    
+  })
 }
