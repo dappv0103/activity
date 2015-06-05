b9d074901311b7967413a073c33ad08fe529a608
Nguyen Thang <thangn.1411@gmail.com>
2015-06-05 09:07:05 +0700

    Update notification.js

--- src/builders/notification.js
+++ src/builders/notification.js
@@ -1,1 +1,13 @@
-
+exports.render = function(docs) {
+  var _results = [];
+  for(var i = 0; i <= docs.length; i++) {
+    _results.push({
+      _id: docs[i]._id,
+      verb: docs[i].verb,
+      actors: docs[i].actors,
+      meta: docs[i].meta,
+      is_read: docs[i].is_read
+    });
+  }
+  return _results;
+}
