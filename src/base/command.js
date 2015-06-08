45642177e8bb20fb7429211795e101bebbb7ccf0
Nguyen Thang <thangn.1411@gmail.com>
2015-06-08 09:31:07 +0700

    Update command.js

--- src/base/command.js
+++ src/base/command.js
@@ -1,30 +1,4 @@
-module.exports = baseCmd;
+module.exports = Command;
 
-function baseCmd() {
-  this.data = null;
-  this.error = null;
+function Command() {
 }
-
-baseCmd.prototype.getData = function() {
-  if(this.data == null) {
-    this.data = this.getError();
-  }  
-  return this.data;
-};
-
-baseCmd.prototype.getError = function() {
-  return this.error;
-};
-
-baseCmd.prototype.setData = function(data) {
-  return this.data = data;
-}
-
-baseCmd.prototype.setError = function(error) {
-  return this.error = error;
-};
-
-
-baseCmd.prototype.getString = function() {
-  return JSON.stringify(this.getData());
-};
