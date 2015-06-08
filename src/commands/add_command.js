49982be909e51f04d93d5a796aff63ff8a296511
Nguyen Thang <thangn.1411@gmail.com>
2015-06-08 09:10:37 +0700

    Update and rename add_feed_cmd.js to add_command.js

---
+++ src/commands/add_command.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+var Feed = mongoose.model('Feed');
+var baseCmd = require('./base_cmd');
+
+module.exports = AddCommand;
+
+
+function AddCommand() {
+  if (!(this instanceof AddCommand)) return new AddCommand();
+  this.data = null;
+  this.error = null;
+};
+
+AddCommand.prototype = baseCmd;
+
+/**
+ * Thêm bảng tin mới
+ * 
+ * @param Object    data
+ * @param Function  callback
+ */
+AddCommand.prototype.run = function (data, callback) {
+  var self = this;
+  var _result = {};
+  var feed = new Feed(data);
+  feed.save(function(err) {
+    if(err) {
+      _result.success = false;
+    } else {
+      _result.success = true;
+      _result.position = feed.position;
+      _result.object = feed.object;
+      _result.meta = feed.meta;
+    }
+    self.data = _result;
+    return callback(self.getString());
+  });
+  
+  this.data = {
+    result: 'ok'
+  };
+  return callback(this.getString());
+};
