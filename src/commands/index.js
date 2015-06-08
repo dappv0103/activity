c3fd3902d1b2781c6c745ab6757fab4907be1b46
Nguyen Thang <thangn.1411@gmail.com>
2015-06-08 09:44:57 +0700

    Update index.js

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,11 +1,9 @@
 var _commands = [];
 
-_commands['add_feed'] = require('./add_feed_cmd');
-_commands['delete_feed'] = require('./delete_feed_cmd');
-_commands['activity'] = require('./activity_cmd');
-_commands['delete_activity'] = require('./delete_activity_cmd');
-_commands['newsfeed'] = require('./newsfeed_cmd');
-_commands['notification'] = require('./notification_cmd');
+_commands['ADD'] = require('./add');
+_commands['DELETE'] = require('./delete');
+_commands['ACTIVITY'] = require('./activity');
+_commands['EDIT'] = require('./edit');
 
 exports.find = function(name) {
   if(_commands[name]) {
