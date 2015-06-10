e947cc43b5195824b901913633a765687f6d6d1a
Nguyen Thang <thangn.1411@gmail.com>
2015-06-10 10:54:02 +0700

    Update index.js

--- src/command/index.js
+++ src/command/index.js
@@ -1,13 +1,37 @@
-var _commands = [];
-
-_commands['ADD'] = require('./add');
-_commands['DELETE'] = require('./delete');
-_commands['ACTIVITY'] = require('./activity');
-_commands['EDIT'] = require('./edit');
-
-exports.find = function(name) {
-  if(_commands[name]) {
-    return _commands[name];
+var Status = require('./status');
+export = {
+  
+ /**
+  * List Command supported
+  */
+ supportedCommands: [
+  'ADD' : "./add",
+  'EDIT' : "./edit",
+  'DELETE' : "./delete",
+  'ACTIVITY' : "./activity"
+ ],
+ 
+ /**
+  * Execute command to the server
+  * 
+  * @param string id Command ID
+  * @param array args Agruments of the command
+  * @param function Callback
+  */
+ exec : function(id, args, callback) {
+  if(this.supportsCommand(id)) {
+    var command = this.getCommand(id);
+    return command.exec(args, callback);
   }
-  return false;
-};
+  return callback(JSON.stringify({
+    status: Status.COMMAND_NOT_SUPPORTED;
+  }));
+ },
+ 
+ /**
+  * Check if support command to the server
+  */
+ supportsCommand : function(id) {
+   return (!this.commands[id]);
+ }
+}
