f0061302d1966a6dc0bcd045c18df3bf9c827428
Nguyen Thang <thangn.1411@gmail.com>
2015-06-10 10:18:08 +0700

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -4,4 +4,7 @@ module.exports = Socket;
 
 function Socket(socket) {
   this.socket = socket;
+  
+  this.socket.on('data', this.onData.bind(this));
+  this.socket.on('end', this.onEnd.bind(this))
 }
